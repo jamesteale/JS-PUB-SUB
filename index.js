d55f482c8d36a1924be3e28d81e581e5d06391bc
James Teale <james.teale@gamesys.co.uk>
2019-05-01 22:54:35 +0100

    Changed order of tools and updated an instruction

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import Palette from './src/colour-palette/Palette.js'
 import Caffeinator from './src/caffeinator/Caffeinator.js';
 import './src/index.scss';
 
-const instructionsToColour = "Select part of the monster to colour, then choose colour.";
+const instructionsToColour = "Select part of the monster, then select a colour";
 const instructionsToCaffeinate = "Click on the cup to dose the monster";
 const toolContainer = document.querySelector('.tools');
 
 const monster = new Monster();
-const caffeinator = new Caffeinator(toolContainer, instructionsToCaffeinate);
 const palette = new Palette(toolContainer, instructionsToColour);
+const caffeinator = new Caffeinator(toolContainer, instructionsToCaffeinate);
+
 
 monster.buildMonster();

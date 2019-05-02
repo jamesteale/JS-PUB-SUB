526fb3ed142c42706ba3fe076560389c2f3ec39b
James Teale <james.teale@gamesys.co.uk>
2019-05-02 07:22:50 +0100

    Added inheritence directory

---
+++ src/inheritence/PubSub.js
@@ -0,0 +1,19 @@
+class PubSub {
+  subscribers = {
+    any: []
+  };
+
+  subscribe(fn, type = "any") {
+    
+  }
+
+  unsubscribe(fn, type) {
+    
+  }
+
+  publish(publication, type = "any") {
+    
+  }
+}
+
+export default PubSub;
